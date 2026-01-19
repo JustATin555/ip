27b707e4ae48edad37d4322e6e55dcfb5741f555
Justin <usingenglish.2014@gmail.com>
2026-01-19 14:50:03 +0800

    Add skeleton hello and goodbye

--- src/main/java/ForgetfulDave.java
+++ src/main/java/ForgetfulDave.java
@@ -1,12 +1,34 @@
 public class ForgetfulDave {
+    // 3D-ASCII Art generated with https://patorjk.com/software/taag/
+    private static final String DAVE_LOGO = " ________  ________  ___      ___ _______      \n"
+        + "|\\   ___ \\|\\   __  \\|\\  \\    /  /|\\  ___ \\     \n"
+        + "\\ \\  \\_|\\ \\ \\  \\|\\  \\ \\  \\  /  / | \\   __/|    \n"
+        + " \\ \\  \\ \\\\ \\ \\   __  \\ \\  \\/  / / \\ \\  \\_|/__  \n"
+        + "  \\ \\  \\_\\\\ \\ \\  \\ \\  \\ \\    / /   \\ \\  \\_|\\ \\ \n"
+        + "   \\ \\_______\\ \\__\\ \\__\\ \\__/ /     \\ \\_______\\\n"
+        + "    \\|_______|\\|__|\\|__|\\|__|/       \\|_______|\n";
+
+    /**
+     * Print a message with a horizontal line underneath
+     * @param msg message to print
+     */
+    private static void printWithLine(String msg) {
+        System.out.println(msg);
+        System.out.println("____________________________________________________________");
+    }
+
+    /**
+     * Main entry point
+     * @param args any terminal arguments for Forgetful Dave
+     */
     public static void main(String[] args) {
-        String logo = " ________  ________  ___      ___ _______      \n"
-                + "|\\   ___ \\|\\   __  \\|\\  \\    /  /|\\  ___ \\     \n"
-                + "\\ \\  \\_|\\ \\ \\  \\|\\  \\ \\  \\  /  / | \\   __/|    \n"
-                + " \\ \\  \\ \\\\ \\ \\   __  \\ \\  \\/  / / \\ \\  \\_|/__  \n"
-                + "  \\ \\  \\_\\\\ \\ \\  \\ \\  \\ \\    / /   \\ \\  \\_|\\ \\ \n"
-                + "   \\ \\_______\\ \\__\\ \\__\\ \\__/ /     \\ \\_______\\\n"
-                + "    \\|_______|\\|__|\\|__|\\|__|/       \\|_______|\n";
-        System.out.println("Hello from\n" + logo);
+        // Show logo at startup
+        System.out.println(DAVE_LOGO);
+
+        // Show welcome message
+        printWithLine("Hello! I'm Duke? Dan? Dave? Something like that...\nHow can I help?");
+
+        // Send goodbye message
+        printWithLine("See you around!");
     }
 }
