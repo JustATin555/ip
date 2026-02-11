2c84b3c3b14e591345bcb18ee607e4fbcfd723db
Justin <usingenglish.2014@gmail.com>
2026-02-11 12:24:38 +0800

    Update index command guards

--- src/main/java/dave/commands/DeleteCommand.java
+++ src/main/java/dave/commands/DeleteCommand.java
@@ -26,12 +26,14 @@ public class DeleteCommand extends Command {
 
     @Override
     public void execute(Ui ui, DiskStore ds, Tasklist tl) {
+        // Guard against invalid index
         int size = tl.getSize();
-        if (index >= size) {
+        if (index < 0 || index >= size) {
             ui.display(String.format("Hmmm, I only remember %d tasks. Which one did you mean?", size));
             return;
         }
 
+        // Remove task
         Task task = tl.remove(index);
         ds.overwrite(tl.getTasks());
         ui.display(String.format("I won't remember this task anymore:\n   %s", task));
--- src/main/java/dave/commands/MarkCommand.java
+++ src/main/java/dave/commands/MarkCommand.java
@@ -26,12 +26,14 @@ public class MarkCommand extends Command {
 
     @Override
     public void execute(Ui ui, DiskStore ds, Tasklist tl) {
+        // Guard against invalid index
         int size = tl.getSize();
-        if (index >= size) {
+        if (index < 0 || index >= size) {
             ui.display(String.format("Hmmm, I only remember %d tasks. Which one did you mean?", size));
             return;
         }
 
+        // Mark task as done
         Task task = tl.setDone(index, true);
         ds.overwrite(tl.getTasks());
         ui.display(String.format("Checked this task off:\n   %s", task));
